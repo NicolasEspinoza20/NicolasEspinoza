22c850063da137717bd06931a4c0a18a9496b9af
Nicolas Espinoza <Nicolas Espinoza@host.docker.internal>
2021-11-27 23:48:43 -0300

    Inicie Proyecto

--- src/bd/Conexion.java
+++ src/bd/Conexion.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bd;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+        
 /**
  *
- * @author Nicolas Espinoza
+ * @author 
+ * @version 23-11-2021
  */
 public class Conexion {
     
+    public Connection obtenerConexion () throws SQLException{
+     
+        
+        Connection connection = null;
+        try {
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tiendadepelicula", "root", "");
+            System.out.println("Conexion exitosa");          
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error de Conexion " + e.getMessage());
+        }
+        
+        return connection;
+    }
+//    public static void main(String[] args) {
+//        Conexion con = new Conexion();
+//        con.obtenerConexion();
+//    }
 }
